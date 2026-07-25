abc281fbd7a139a6d876470cac8471cf2778bb7b
SHETTY1941 <kingtech1969@gmail.com>
2026-07-26 01:49:10 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -36,4 +36,8 @@ app.delete("/api/contacts/:id",async (req,res)=>{
     res.send({message: 'deleted'});
 });
 
-app.listen(5000,()=>console.log("server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
